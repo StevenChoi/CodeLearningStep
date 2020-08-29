f08056bbafccfbc5bcac520d580e5e494cfc8f45
崔余丰 <122246729@qq.com>
2020-08-30 01:44:32 +0800

    Python400_03

---
+++ Python/03.py
@@ -0,0 +1,14 @@
+import turtle
+
+turtle.showturtle()
+turtle.write('Steven')
+turtle.forward(300)
+turtle.color('red')
+turtle.left(90)
+turtle.forward(300)
+turtle.goto(0, 50)
+turtle.goto(0, 0)
+turtle.penup()
+turtle.goto(0, 300)
+turtle.pendown()
+turtle.circle(100)
